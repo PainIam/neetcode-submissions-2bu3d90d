b7d2440822433df5029df2bd856cf839b2c53d68
Molete <153856614+PainIam@users.noreply.github.com>
2026-04-06 23:30:49 +0200

    Add: longest-substring-without-duplicates - submission-8

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+
+        int mx = 0;
+        int l = 0, r = 0;
+        unordered_map<int, int> map;
+        while (r < s.size()) {
+
+            if (map.count(s[r])) {
+                l = max (l, map[s[r]] + 1);
+            }
+            map[s[r]] = r;
+            mx = max(mx, r - l + 1);
+            r++;
+        }
+
+        return mx;
+    }
+};
